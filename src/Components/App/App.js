191842e4a0104890c0d54ed10217be247b53b2e7
Kristen Bair <kristenmbair@gmail.com>
2021-03-03 19:51:59 -0700

    Create landing page basic build, add router functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import CardDetails from '../CardDetails/CardDetails'
 import Footer from '../Footer/Footer'
 import Info from '../Info/Info'
@@ -10,15 +11,31 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <CardDetails />
-      <Footer />
-      <Info />
-      <Intention />
-      <LandingPage />
-      <Reading />
-      <SavedReadings />
-    </>
+    <Switch>
+
+      <Route        
+        exact path='/'
+        render={() => {
+          return <LandingPage />
+        }}
+      />      
+      <Route 
+        path='/info'
+        component={ Info }
+      />
+      <Route 
+        path='/intention'
+        component={ Intention }
+      />
+        <CardDetails />
+     
+        <Footer />
+              
+        <Reading />
+    
+        <SavedReadings />
+      
+    </Switch>
   );
 }
 
--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './LandingPage.css'
 
 function LandingPage() {
   return (
-    <h1>LandingPage</h1>
+    <section className="landing-page-section">
+      <h1>Welcome</h1>
+      <img 
+        src=""
+        alt=""
+        className="welcome-img"
+      />
+      <Link to='/info'>
+        <button>How to Start</button>
+      </Link>
+      <Link to='/intention'>
+        <button>Begin a Reading</button>
+      </Link>
+    </section>
   );
 }
 
