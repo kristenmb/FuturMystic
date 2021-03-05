7f9e1b9cf35dc640c32f2e82acb945b6f04850bf
Kristen Bair <kristenmbair@gmail.com>
2021-03-04 22:02:22 -0700

    Update intention page content and styling

--- src/Components/Intention/Intention.js
+++ src/Components/Intention/Intention.js
@@ -5,8 +5,8 @@ import './Intention.css'
 function Intention() {
   return (
     <section className="intention-section">
-      <h1>Set your intention for your reading</h1>
-      <p></p>
+      <h1>Set your intentions</h1>
+      <p>Take three deep breaths and focus on what you want to know</p>
       <Link to='/reading'>
       <button className='reading-btn'>Begin</button>
       </Link>
