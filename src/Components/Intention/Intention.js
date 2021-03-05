6d8b117aa402d7a0d5cb9ab1ec8ba01c21edde0f
Kristen Bair <kristenmbair@gmail.com>
2021-03-04 21:49:29 -0700

    Add layout and styling for intentions page

--- src/Components/Intention/Intention.js
+++ src/Components/Intention/Intention.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Intention.css'
 
 function Intention() {
   return (
-    <h1>Intention</h1>
+    <section className="intention-section">
+      <h1>Set your intention for your reading</h1>
+      <p></p>
+      <Link to='/reading'>
+      <button className='reading-btn'>Begin</button>
+      </Link>
+    </section>
   );
 }
 
