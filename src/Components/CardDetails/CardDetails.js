2f8ff83ad931418e78f6e83706a1d40e39ac4ec0
Kristen Bair <kristenmbair@gmail.com>
2021-03-05 22:18:04 -0700

    Add styling and spacing for details view

--- src/Components/CardDetails/CardDetails.js
+++ src/Components/CardDetails/CardDetails.js
@@ -1,21 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import arrow from '../../assets/backarrow.png'
 import { cardImages } from '../../cardImageData'
 import './CardDetails.css'
 
 function CardDetails({ selectedCard }) {
   return (
-    <section className="details-section">
-      <img
-        className="details-img"
-        src={`.${cardImages[selectedCard.name_short].src}`}
-        alt={cardImages[selectedCard.name_short].alt}
-      />
-      <article className="details-text">
-        <h1>{selectedCard.name}</h1>
-        <p>Meaning: {selectedCard.meaning_up}</p>
-        <p>Reversed: {selectedCard.meaning_rev}</p>
-      </article>
-    </section>
+    <>
+      <header className="details-header">
+        <Link to='/reading'>
+          <img
+            className="back-arrow"
+            src={arrow}
+            alt="Arrow pointing left"
+          />
+        </Link>
+      </header>
+      <section className="details-section">
+        <img
+          className="details-img"
+          src={`.${cardImages[selectedCard.name_short].src}`}
+          alt={cardImages[selectedCard.name_short].alt}
+        />
+        <article className="details-text">
+          <h1>{selectedCard.name}</h1>
+          <h2>Meaning:</h2>
+          <p>{selectedCard.meaning_up}</p>
+          <h2>Reversed:</h2>
+          <p>{selectedCard.meaning_rev}</p>
+        </article>
+      </section>
+    </>
   );
 }
 
