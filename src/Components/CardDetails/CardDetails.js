763dc85d887196ae8c0bc93d792da19dac6af476
Kristen Bair <kristenmbair@gmail.com>
2021-03-05 21:39:00 -0700

    Add display of card details from api data

--- src/Components/CardDetails/CardDetails.js
+++ src/Components/CardDetails/CardDetails.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { cardImages } from '../../cardImageData'
 import './CardDetails.css'
 
 function CardDetails({ selectedCard }) {
-  console.log(selectedCard)
   return (
-    <h1>{selectedCard.name}</h1>
+    <section className="details-section">
+      <img
+        className="details-img"
+        src={`.${cardImages[selectedCard.name_short].src}`}
+        alt={cardImages[selectedCard.name_short].alt}
+      />
+      <article className="details-text">
+        <h1>{selectedCard.name}</h1>
+        <p>Meaning: {selectedCard.meaning_up}</p>
+        <p>Reversed: {selectedCard.meaning_rev}</p>
+      </article>
+    </section>
   );
 }
 
