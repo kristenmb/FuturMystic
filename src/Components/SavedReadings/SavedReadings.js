40955f593d615d11ff88e06c0dd5723004e8b7dc
Kristen Bair <kristenmbair@gmail.com>
2021-03-05 23:05:45 -0700

    Create link to change url to saved readings path

--- src/Components/SavedReadings/SavedReadings.js
+++ src/Components/SavedReadings/SavedReadings.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './SavedReadings.css'
 
-function SavedReadings() {
+function SavedReadings({ userSavedReadings }) {
+  console.log(userSavedReadings)
+  
   return (
     <h1>SavedReadings</h1>
   );
