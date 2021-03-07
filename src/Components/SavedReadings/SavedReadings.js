dc3d5d74ae4486cb0af65f31a38b0f1d5115bbd0
Kristen Bair <kristenmbair@gmail.com>
2021-03-06 19:09:36 -0700

    Create controlled input form for comment section below saved readings, and display images that match saved reading cards

--- src/Components/SavedReadings/SavedReadings.js
+++ src/Components/SavedReadings/SavedReadings.js
@@ -1,5 +1,5 @@
-import { render } from '@testing-library/react'
 import React, { Component } from 'react'
+import { cardImages } from '../../cardImageData'
 import './SavedReadings.css'
 
 class SavedReadings extends Component {
@@ -9,31 +9,38 @@ class SavedReadings extends Component {
       userSavedReadings: userSavedReadings
     }
   }
-  
-  componentDidMount() {
-    
-  }
 
   handleChange = (event) => {
     const { id, value} = event.target
     
     this.setState(prevState => ({
-    userSavedReadings: prevState.userSavedReadings.map(
-    item => {
+    userSavedReadings: prevState.userSavedReadings.map(item => {
       const readingDate = Object.keys(item)
       return (readingDate[0] === id ? Object.assign(item, { comments: value }) : item)
     })
-  }))
-}
+    }))
+  }
 
   readingBoxes = () => {
     return this.state.userSavedReadings.map((reading, i) => {
       const getDate = parseInt(Object.keys(reading))
       const reformatDate = new Date(getDate)
       const date = `${reformatDate.getMonth() + 1}/${reformatDate.getDate()}/${reformatDate.getFullYear()}`
+      const cardThumbnails = reading[getDate].map(card => {
+        return (
+          <img
+            src={cardImages[card.name_short].src}
+            alt={cardImages[card.name_short].alt}
+            className="thumbnails"
+          />
+        )
+        })
       return (
         <article className="reading-box" key={i}>
           <h2>{date}</h2>
+          <div>
+            {cardThumbnails}
+          </div>
           <form>
             <textarea
               name="comments"
@@ -48,10 +55,13 @@ class SavedReadings extends Component {
   }
 
   render() {
-    console.log(this.state.userSavedReadings)
     return (
-      <section>
-        {this.readingBoxes()}
+      <section className="saved-section">
+        <h1>My Saved Readings</h1>
+        <p>Add notes about your reading here, context</p>
+        <div className="saved-container">
+          {this.readingBoxes()}
+        </div>
       </section>
     );
   }
