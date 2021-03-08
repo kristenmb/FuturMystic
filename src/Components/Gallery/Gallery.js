94125d5d56c19ff46659be0659033e24d0d626d5
Kristen Bair <kristenmbair@gmail.com>
2021-03-08 16:42:34 -0700

    Update getCardDetails function to include appropriate arguments when called

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import Image from '../Image/Image'
+import Footer from '../Footer/Footer'
 import { Link } from 'react-router-dom'
 import './Gallery.css'
 
 const Gallery = ({ fullDeck, getCardDetails }) => {
   const cardThumbnails = fullDeck.map((card, i) => {
      return (
-     <Link to={`/reading/${card.name}`}  key={i} >
+     <Link to={`/gallery/${card.name}`}  key={i} >
         <article>
           <Image
             imageName={card.name_short}
             className={"gallery-thumbnail"}
             id={i+1}
-            onClick={(event) => getCardDetails(event)}
+            onClick={(event) => getCardDetails('fullDeck', 'gallery', event)}
           />
+          <Footer />
         </article>
       </Link>
      )
