3a92dd695e62ba91ce8c4eab262671fb6f12a4ea
Kristen Bair <kristenmbair@gmail.com>
2021-03-07 19:22:51 -0700

    Reset url in api call

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 export const fetchCards = () => {
-  return fetch('https://rws-cards-api.herokuapp.com/api/v1/cards/ranom?n=3')
+  return fetch('https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=3')
     .then(response => {
       handleErrors(response)
       return response.json()
