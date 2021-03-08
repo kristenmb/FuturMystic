4a19cf79c2bb685d506b73b1983bedcd530955a9
Kristen Bair <kristenmbair@gmail.com>
2021-03-08 16:09:57 -0700

    Add gallery render for all cards

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import Image from '../Image/Image'
+import { Link } from 'react-router-dom'
+import './Gallery.css'
 
-const Gallery = () => {
+const Gallery = ({ fullDeck, getCardDetails }) => {
+  const cardThumbnails = fullDeck.map((card, i) => {
+     return (
+     <Link to={`/reading/${card.name}`}  key={i} >
+        <article>
+          <Image
+            imageName={card.name_short}
+            className={"gallery-thumbnail"}
+            id={i+1}
+            onClick={(event) => getCardDetails(event)}
+          />
+        </article>
+      </Link>
+     )
+  })
+
   return (
-    <h1>gallery</h1>
+    <section className="gallery-section">
+      <h1 className="gallery-title">Card Gallery</h1>
+      <p className="gallery-desc">Click through the cards to familiarize yourself with them.</p>
+      <article className="thumbnail-container">
+        {cardThumbnails}
+      </article> 
+    </section>
   )
 }
 
